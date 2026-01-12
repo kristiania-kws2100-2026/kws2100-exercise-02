95b4ca2cf7f996f4ad0a824db9df308cf1505d5a
Johannes Brodwall <jhannes@gmail.com>
2026-01-12 15:54:21 +0100

    start Thinking in React

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(<h1>Hello React</h1>);
+createRoot(document.getElementById("root")).render(<App />);
